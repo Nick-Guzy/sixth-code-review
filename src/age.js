18add3e5f53b0a7f7084d7a1924ab399123ea558
nicksfed <nick.guzy76@gmail.com>
2023-02-10 12:32:29 -0800

    set up environment

--- src/age.js
+++
@@ -1,7 +0,0 @@
-export default class Person {
-  constructor(currentAge, pastAge, futureAge) {
-  this.currentAge = currentAge;
-  this.pastAge = pastAge;
-  this.futureAge = futureAge;
-  }
-};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import Person from "./age";
 
-function handleAgeForm(event) {
-  window.addEventListener("load", function());
-    addEventListener("click", handleAgeForm);
-  event.preventDefault();
-  const currentAge = document.querySelector("age-checker-form").value;
-  const pastAge = 
-  myPerson = new Person(currentAge, pastAge, futureAge);
-  const calcAge = myPerson.checkType();
-
-}
