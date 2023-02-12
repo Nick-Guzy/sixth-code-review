0f791410c6c79d8bc46e65254de3a42f559591cd
nicksfed <nick.guzy76@gmail.com>
2023-02-11 21:07:51 -0800

    Add print converted value to the screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
+import CurrencyService from "./js/currency.js";
 
+//Business logic
+
+async function getCurrency(amount, currency) {
+  const response = await CurrencyService.getMoney(amount, currency);
+  if (response.conversion_result) {
+    displayAnswer(response, currency);
+  } else {
+    displayError(response, currency);
+  }
+}
+
+//UI logic
+
+function displayResult(event) {
+  event.preventDefault();
+  const amount = document.getElementById("dollarInput").value;
+  const currency = document.getElementById("currencySelect").value;
+  getCurrency(amount, currency);
+}
+
+function displayAnswer(response, currency) {
+  const output = response.conversion_result;
+  document.getElementById(
+    "money"
+  ).innerText = `You have ${output} in ${currency}`;
+}
+
+function displayError(error, currency) {
+  document.getElementById(
+    "error"
+  ).innerText = `There was an error getting the currency from ${currency}: ${error}.`;
+}
+
+window.addEventListener("load", function () {
+  document
+    .getElementById("currencyForm")
+    .addEventListener("submit", displayResult);
+});
--- src/js/currency.js
+++ src/js/currency.js
@@ -1,15 +1,14 @@
 //Business logic
 export default class CurrencyService {
-  static async getMoney(currency, amount) {
+  static async getMoney(amount, currency) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}
-      /pair/USD/${currency}/${amount}`);
-      const jsonifiedResponse = await response.json();
+      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`);
+      const jsonResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statustext}`;
         throw new Error(errorMessage);
       }
-      return jsonifiedResponse;
+      return jsonResponse;
     } catch (error) {
       return error;
     }
